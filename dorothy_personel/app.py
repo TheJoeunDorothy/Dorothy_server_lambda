09f4cc53ad119c2d8246c5fd99ca51aa2d1b9c1f
Oh-Kang94 <okh19941994@naver.com>
2023-11-13 11:37:45 +0900

    #003 add annotation and sub unused system lib

--- dorothy_personel/app.py
+++ dorothy_personel/app.py
@@ -8,53 +8,54 @@ import json
 import dlib
 
 def lambda_handler(event, context):
+    warnings.filterwarnings('ignore')
     if not "body" in event or len(event["body"]) == 0:
         return {"statusCode": 400, "body": json.dumps({"result": 'No Body'})}
     image_data = event['body']
-    print("Image data received")  # 로그 추가
 
-    # Base64 디코딩을 통해 이미지를 복원
-    image = base64.b64decode(image_data)
-    print("Image decoded from base64")  # 로그 추가
-
-    # Pillow를 사용하여 이미지로 디코딩
-    image = Image.open(BytesIO(image))
-    print("Image decoded with Pillow")  # 로그 추가
-
-    # 이미지를 numpy 배열로 변환
-    image = np.array(image)
-    print("Image converted to numpy array")  # 로그 추가
-
+    '''이미지 처리 및 디코딩 '''
     try:
-        warnings.filterwarnings('ignore')
+        # Base64 디코딩을 통해 이미지를 복원
+        image = base64.b64decode(image_data)
 
+        # Pillow를 사용하여 이미지로 디코딩
+        image = Image.open(BytesIO(image))
+
+        # 이미지를 numpy 배열로 변환
+        image = np.array(image)
+    except Exception as e:
+        print(e)
+        return {"statusCode": 400, "body": json.dumps({"result": "Can't decode image or make numpy"})}
+
+    '''얼굴 인식 및 처리 '''
+    try:
         # 얼굴 검출기 초기화
         face_detector = dlib.get_frontal_face_detector()
-        print("Face detector initialized")  # 로그 추가
 
         # 얼굴 영역 검출
         faces = face_detector(image)
-        print("Faces detected")  # 로그 추가
 
         # 얼굴 랜드마크 검출기 초기화
         landmark_predictor = dlib.shape_predictor(
             '/var/task/static/shape_predictor_68_face_landmarks.dat')
-        print("Landmark predictor initialized")  # 로그 추가
-
+        
+    except Exception as e:
+        print(e)
+        return {"statusCode": 400, "body": json.dumps({"result": "Can't find face"})}
+    
+    '''이미지 ML 처리 '''
+    try:
         for face in faces:
             # 얼굴 랜드마크 검출
             landmarks = landmark_predictor(image, face)
-            print("Landmarks detected")  # 로그 추가
 
             # 미간 자리 좌표 추출 (27부터 30까지의 인덱스 사용)
             forehead_points = np.array(
                 [(landmarks.part(i).x, landmarks.part(i).y) for i in range(27, 31)])
-            print("Forehead points extracted")  # 로그 추가
 
             # 해당 영역의 색상 추출
             forehead_color_rgb = np.mean(
                 image[forehead_points[:, 1], forehead_points[:, 0]], axis=0)
-            print("Forehead color extracted")  # 로그 추가
 
             r = forehead_color_rgb[0]
             g = forehead_color_rgb[1]
@@ -63,7 +64,8 @@ def lambda_handler(event, context):
             pre = clf.predict([[r, g, b]])
 
     except Exception as e:
-        return {"statusCode": 400, "body": json.dumps({"result": str(e)})}
+        print(e)
+        return {"statusCode": 400, "body": json.dumps({"result": "Can't predict Personel Color"})}
         
     return {"statusCode": 200, "body": json.dumps({"result": pre[0]})}
 
